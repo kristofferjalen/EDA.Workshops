58eefcd57c75350caa43a981993c303999b6c2d4
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2020-05-28 08:38:32 +0200

    Implement GameState

--- src/1_rps/RPS.Tests/GameState.cs
+++ src/1_rps/RPS.Tests/GameState.cs
@@ -3,7 +3,16 @@
     public class GameState
     {
         public GameState When(IEvent @event) => this;
+
         public GameStatus Status { get; set; }
+
+        public GameState When(GameCreated @event) => new GameState { Status = GameStatus.ReadyToStart };
+
+        public GameState When(RoundStarted @event) => new GameState { Status = GameStatus.Started };
+
+        public GameState When(GameStarted @event) => new GameState { Status = GameStatus.Started };
+
+        public GameState When(GameEnded @event) => new GameState { Status = GameStatus.Ended };
     }
 
 }
